ab36fb4ae474487e72c55f1839efb7433b92fbd7
Marcin <mch.wizard@gmail.com>
2023-02-12 19:05:43 +0100

    We start creating a campaign component

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ethers } from 'ethers';
+import { money } from '../assets';
+import { CustomButton } from '../components';
+import { checkIfImage } from '../utils';
 
 const CreateCampaign = () => {
   return (
